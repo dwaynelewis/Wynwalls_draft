bc7ec0b6b27720b4d564926d09aa6630388c6a05
Dwayne Richard Lewis <dwayne@fashiongatekeepers.com>
2017-09-17 14:55:51 -0400

    solo portion of project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+end
+def edit
+  @article =
+  Article.find(params[:id])
+end
+  def update
+    @article = Article.find(params[:id])
+end
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
