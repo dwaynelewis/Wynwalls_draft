6562c21fa3f2dcc396404b6ca2c3bfd7f46df98a
Dwayne Richard Lewis <dwayne@fashiongatekeepers.com>
2017-09-18 15:27:46 -0400

    updated index & new views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(:article)
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
